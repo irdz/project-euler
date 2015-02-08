78a6373debc1f1601035ad0523f786878a922f96
Ivan Rodriguez <ivan.rodriguez001@gmail.com>
2015-02-08 12:20:37 -0800

    Added the java folder

---
+++ java/prime-factor-1/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+        // Find the sum of all the multiples of 3 or 5 below 1000.
+
+        int sum = 0;
+
+        for (int i = 0; i < 1001; i++) {
+            if (i % 3 == 0 || i % 5 == 0 ) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
